73b1c4a894ae29636c3e783151d89a0a85b6ab76
RobertoJimenez <RobertoJimenez@NB-114.energia-chile.local>
2017-10-10 12:15:27 -0300

    Version 2 del proeycto

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -17,6 +17,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         // TODO code application logic here
         // hola a todos
+        // queso
     }
     
 }
